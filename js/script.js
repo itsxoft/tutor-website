412335754275b3dfe106b7cf2036f677c8ae2553
Xoft <itsxoft@gmail.com>
2026-01-03 21:44:14 +0100

    Added navbar background effect and finctionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Toggle mobile menu
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
   const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
@@ -5,4 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
   toggleButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
   });
+});
+
+// Change navbar background on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar-scroll');
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
 });
